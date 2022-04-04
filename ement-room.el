95262e85710f9de0bc38ff111438291bdfb0850b
Adam Porter <adam@alphapapa.net>
2022-04-04 04:22:55 -0500

    Change: (ement-room-mark-read) Ensure proper major mode
    See #59.

--- ement-room.el
+++ ement-room.el
@@ -2074,15 +2074,18 @@ FULLY-READ-EVENT.
 
 Interactively, mark both types as read up to event at point."
   (declare (indent defun))
-  (interactive (let* ((node (ewoc-locate ement-ewoc))
-                      (event (cl-typecase (ewoc-data node)
-                               (ement-event (ewoc-data node))
-                               (t (when-let ((prev-event-node (ement-room--ewoc-next-matching ement-ewoc node
-                                                                #'ement-event-p #'ewoc-prev)))
-                                    (ewoc-data prev-event-node))))))
-                 (list ement-room ement-session
-                       :read-event event
-                       :fully-read-event event)))
+  (interactive
+   (cl-assert (equal 'ement-room-mode major-mode) nil
+              "This command is to be used in `ement-room-mode' buffers")
+   (let* ((node (ewoc-locate ement-ewoc))
+          (event (cl-typecase (ewoc-data node)
+                   (ement-event (ewoc-data node))
+                   (t (when-let ((prev-event-node (ement-room--ewoc-next-matching ement-ewoc node
+                                                    #'ement-event-p #'ewoc-prev)))
+                        (ewoc-data prev-event-node))))))
+     (list ement-room ement-session
+           :read-event event
+           :fully-read-event event)))
   (cl-assert room) (cl-assert session) (cl-assert (or read-event fully-read-event))
   (if (not fully-read-event)
       ;; Sending only a read receipt, which uses a different endpoint
