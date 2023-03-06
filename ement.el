alphapapa/ement.el

commit 850f6b277226a6b164cce30ccaf6aa499fe46fe4

Fix: (ement--sync) Retry for HTTP 502 errors
Thanks to Simon Pugnet (@polaris64) for reporting.

README.org
@@ -295,6 +295,9 @@ Note that, while ~matrix-client~ remains usable, and probably will for some time
 *Changes*
 + Improve ~ement-describe-room~ command (formatting, bindings).
 
+*Fixes*
++ Retry sync for HTTP 502 "Bad Gateway" errors.
+
 ** 0.5.2
 
 *Fixes*

ement.el
@@ -450,21 +450,29 @@ a filter ID).  When unspecified, the value of
                           :then (apply-partially #'ement--sync-callback session)
                           :else (lambda (plz-error)
                                   (setf (map-elt ement-syncs session) nil)
-                                  (pcase (plz-error-curl-error plz-error)
-                                    (`(,(or 28 429 502) . ,_)
-                                     ;; Timeout (28), "Too Many Requests" (429), or "Bad Gateway" (502): sync again if enabled.
-                                     (if (not ement-auto-sync)
-                                         (run-hook-with-args 'ement-interrupted-sync-hook session)
-                                       (let ((reason (pcase-exhaustive (car (plz-error-curl-error plz-error))
-                                                       (28 "timed out")
-                                                       ((or 429 502) "failed"))))
-                                         (message "Ement: Sync %s (%s).  Syncing again..."
-                                                  reason (ement-user-id (ement-session-user session))))
-                                       ;; Set QUIET to allow the just-printed message to remain visible.
-                                       (ement--sync session :timeout timeout :quiet t)))
-                                    (`(,code . ,message)
-                                     (signal 'ement-api-error (list (format "Ement: Network error: %s: %s" code message) plz-error)))
-                                    (_ (signal 'ement-api-error (list "Ement: Unrecognized network error" plz-error)))))
+                                  ;; TODO: plz probably needs nicer error handling.
+                                  ;; Ideally we would use `condition-case', but since the
+                                  ;; error is signaled in `plz--sentinel'...
+                                  (pcase-let (((cl-struct plz-error curl-error response) plz-error)
+                                              (reason))
+                                    (cond ((when response
+                                             (pcase (plz-response-status response)
+                                               ((or 429 502) (setf reason "failed")))))
+                                          ((pcase curl-error
+                                             (28 (setf reason "timed out")))))
+                                    (if reason
+                                        (if (not ement-auto-sync)
+                                            (run-hook-with-args 'ement-interrupted-sync-hook session)
+                                          (message "Ement: Sync %s (%s).  Syncing again..."
+                                                   reason (ement-user-id (ement-session-user session)))
+                                          ;; Set QUIET to allow the just-printed message to remain visible.
+                                          (ement--sync session :timeout timeout :quiet t))
+                                      ;; Unrecognized errors:
+                                      (pcase curl-error
+                                        (`(,code . ,message)
+                                         (signal 'ement-api-error (list (format "Ement: Network error: %s: %s" code message)
+                                                                        plz-error)))
+                                        (_ (signal 'ement-api-error (list "Ement: Unrecognized network error" plz-error)))))))
                           :json-read-fn (lambda ()
                                           "Print a message, then call `json-read'."
                                           (when (ement--sync-messages-p session)

commit 66b32b9e17a45836f608dca4e082df9b0766adad

Fix: (ement--sync) Retry for network timeouts
Accidentally broken in 850f6b277226a6b164cce30ccaf6aa499fe46fe4.

README.org
@@ -303,7 +303,8 @@ Note that, while ~matrix-client~ remains usable, and probably will for some time
 *Fixes*
 
 + Command ~ement-view-space~ when called from a room buffer.  (Thanks to [[https://github.com/MagicRB][Richard Brežák]] for reporting.)
-+ Don't call ~display-buffer~ when reverting room list buffer.  (Fixes [[https://github.com/alphapapa/ement.el/issues/121][#121]].  Thanks to [[https://github.com/mekeor][mekeor]] for reporting.) 
++ Don't call ~display-buffer~ when reverting room list buffer.  (Fixes [[https://github.com/alphapapa/ement.el/issues/121][#121]].  Thanks to [[https://github.com/mekeor][mekeor]] for reporting.)
++ Retry sync for network timeouts.  (Accidentally broken in v0.6.) 
 
 *Internal*
 

ement.el
@@ -461,7 +461,7 @@ a filter ID).  When unspecified, the value of
                                              (pcase (plz-response-status response)
                                                ((or 429 502) (setf reason "failed")))))
                                           ((pcase curl-error
-                                             (28 (setf reason "timed out")))))
+                                             (`(28 . ,_) (setf reason "timed out")))))
                                     (if reason
                                         (if (not ement-auto-sync)
                                             (run-hook-with-args 'ement-interrupted-sync-hook session)