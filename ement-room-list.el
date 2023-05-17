alphapapa/ement.el

commit 11450ed3440741730581d97c82880cb51d9ff489

Add/Change: (ement-room-list-button-map)
* ement-room-list.el
(ement-room-list-mouse-ement-room-list-kill-buffer): New command.
(ement-room-list-mode-map): Bind new command.
(ement-room-list-button-map): New keymap variable.
(column "Name"): Apply new keymap to buttons.

README.org
@@ -301,6 +301,7 @@ Note that, while ~matrix-client~ remains usable, and probably will for some time
 
 + Option ~ement-room-timestamp-header-align~ controls how timestamp headers are aligned in room buffers.
 + Option ~ement-room-view-hook~ runs functions when ~ement-room-view~ is called.  (By default, it refreshes the room list buffer.)
++ In the room list, middle-clicking a room which has a buffer closes its buffer.
 
 *Changes*
 

ement-room-list.el
@@ -66,9 +66,22 @@ a symbol, it should be unquoted.."
     (define-key map (kbd "SPC") #'ement-room-list-next-unread)
     (define-key map [tab] #'ement-room-list-section-toggle)
     (define-key map [mouse-1] (ement-room-list-define-mouse-command ement-room-list-RET))
+    (define-key map [mouse-2] (ement-room-list-define-mouse-command ement-room-list-kill-buffer))
     (define-key map (kbd "k") #'ement-room-list-kill-buffer)
     (define-key map (kbd "s") #'ement-room-toggle-space)
-    map))
+    map)
+  "Keymap for `ement-room-list' buffers.
+See also `ement-room-list-button-map'.")
+
+(defvar ement-room-list-button-map
+  ;; This map is needed because some columns are propertized as buttons, which override
+  ;; the main keymap.
+  ;; TODO: Is it possible to adjust the button properties to obviate this map?
+  (let ((map (make-sparse-keymap)))
+    (define-key map [mouse-1] (ement-room-list-define-mouse-command ement-room-list-RET))
+    (define-key map [mouse-2] (ement-room-list-define-mouse-command ement-room-list-kill-buffer))
+    map)
+  "Keymap for buttonized text in `ement-room-list' buffers.")
 
 (defvar ement-room-list-timestamp-colors nil
   "List of colors used for timestamps.
@@ -386,9 +399,10 @@ from recent to non-recent for rooms updated in the past hour.")
              (push 'ement-room-list-invited (map-elt face :inherit)))
             ('leave
              (push 'ement-room-list-left (map-elt face :inherit))))
-          (propertize (ement--button-buttonize display-name #'ement-room-list-mouse-1)
+          (propertize display-name
                       'face face
-                      'mouse-face 'highlight))
+                      'mouse-face 'highlight
+                      'keymap ement-room-list-button-map))
         "")))
 
 (ement-room-list-define-column #("Unread" 0 6 (help-echo "Unread events (Notifications:Highlights)")) (:align 'right)

commit 91590202d13623830491ada3bafb03f8b1eb3cc4

Fix: (ement-room-list-next-unread)
Was broken by 11450ed3440741730581d97c82880cb51d9ff489.

Fixes #149.  Thanks to @jeffbowman for reporting.

README.org
@@ -300,6 +300,7 @@ Note that, while ~matrix-client~ remains usable, and probably will for some time
 
 *Fixes*
 + Another attempt at restoring position in room list when refreshing.
++ Command ~ement-room-list-next-unread~.
 
 ** 0.9.2
 

ement-room-list.el
@@ -753,19 +753,17 @@ left."
 (defun ement-room-list-next-unread ()
   "Show next unread room."
   (interactive)
-  (unless (button-at (point))
-    (call-interactively #'forward-button))
+  (when (eobp)
+    (goto-char (point-min)))
   (unless (cl-loop with starting-line = (line-number-at-pos)
                    for value = (oref (magit-current-section) value)
-                   for room = (elt value 0)
-                   for session = (elt value 1)
-                   if (ement--room-unread-p room session)
+                   if (and (vectorp value)
+                           (ement--room-unread-p (elt value 0) (elt value 1)))
                    do (progn
-                        (goto-char (button-end (button-at (point))))
-                        (push-button (1- (point)))
+                        (ement-view-room (elt value 0)  (elt value 1))
                         (ement-room-goto-fully-read-marker)
                         (cl-return t))
-                   else do (call-interactively #'forward-button)
+                   else do (forward-line 1)
                    while (> (line-number-at-pos) starting-line))
     ;; No more unread rooms.
     (message "No more unread rooms")))